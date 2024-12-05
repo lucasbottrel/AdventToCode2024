77773434b0af141a456040158e261d8fceca4f44
Lucas Bottrel <lucas.moura@ex.gerdau.com>
2024-12-05 14:40:03 -0300

    File Reader

--- utils/fileReader.js
+++ utils/fileReader.js
@@ -1,19 +1,17 @@
-const fs = require("fs");
-const path = require("path");
+import { readFileSync } from "fs";
+import { resolve } from "path";
 
 /**
  * Lê o conteúdo de um arquivo de texto.
  * @param {string} filePath Caminho para o arquivo de input.
  * @returns {string} Conteúdo do arquivo como string.
  */
-const readInput = (filePath) => {
+export const readInput = (filePath) => {
     try {
-        const absolutePath = path.resolve(filePath); // Resolve o caminho absoluto
-        return fs.readFileSync(absolutePath, "utf8").trim(); // Lê e remove espaços extras
+        const absolutePath = resolve(filePath); // Resolve o caminho absoluto
+        return readFileSync(absolutePath, "utf8").trim(); // Lê e remove espaços extras
     } catch (error) {
         console.error(`Erro ao ler o arquivo: ${filePath}`);
         throw error;
     }
 };
-
-module.exports = { readInput };
